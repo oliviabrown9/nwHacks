9c6f6441cb9b408a1902c775a218255abcae200f
Olivia <obrown917@gmail.com>
2018-01-13 15:28:44 -0800

    Storyboard buttons

---
+++ nwHacks/CreateEmergencyViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  nwHacks
+//
+//  Created by Olivia Brown on 1/13/18.
+//  Copyright © 2018 Olivia Brown. All rights reserved.
+//
+
+import UIKit
+
+class CreateEmergencyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    private func createEmergency() {
+        // find nearest call center
+        // send user info to call center
+        // call 911
+    }
+
+    @IBAction func fireButtonPressed(_ sender: Any) {
+    }
+    @IBAction func medicalButtonPressed(_ sender: Any) {
+    }
+    @IBAction func policeButtonPressed(_ sender: Any) {
+    }
+    @IBAction func carCrashButtonPressed(_ sender: Any) {
+    }
+    
+}
+
