d3757e73e8dd2bcc6605085897c18fdbd133bca8
Olivia <obrown917@gmail.com>
2018-01-13 23:04:33 -0800

    Added widget

---
+++ nwHacksWidget/TodayViewController.swift
@@ -0,0 +1,31 @@
+//
+//  TodayViewController.swift
+//  nwHacksWidget
+//
+//  Created by Olivia Brown on 1/13/18.
+//  Copyright © 2018 Olivia Brown. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    @IBAction func fireButtonPressed(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view from its nib.
+    }
+    
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+}
